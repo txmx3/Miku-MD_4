bb53fff506eae7f2406304c6e589f2ae26652b2d
txmx3 <116598451+txmx3@users.noreply.github.com>
2023-01-13 16:57:51 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,27 +7,27 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.OwnerNumber = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.ownertag = ['918101187835']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
-global.BotSourceCode = "https://github.com/FantoX001/Miku-MD" // -- Please don't change -- //
+global.Owner = ['4917662927731','491777062822']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.OwnerNumber = ['4917662927731','491777062822']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.ownertag = ['4917662927731','491777062822']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.BotName = "Mika"
+global.packname = "Mika bot"
+global.author = "By: Max_2"
+global.OwnerName = "M&K Bots Inc."
+global.BotSourceCode = "https://github.com/txmx3/Miku-MD_4" // -- Please don't change -- //
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"  // -- Please don't change -- //
 global.sessionName = "session"  // -- Don't change otherwise bot will not work -- //
 
 
 
-global.prefa = ['-'] // Prefix ---  change it to anythong you want except '@' //
+global.prefa = ['#'] // Prefix ---  change it to anythong you want except '@' //
 
 
-global.location = "West Bengal, India"
+global.location = "Berlin,Germany"
 global.reactmoji = "❤️"
 global.themeemoji = "💖"
 global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
-global.websitex = "https://github.com/FantoX001"
+global.websitex = "bumpee.co/aadv8w9mnho"
 global.lolhuman = "KaysaS"
 
 
